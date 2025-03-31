0da74c38a5a25dcbb97e4f7ea0029799f8dd009c
lordrosta-r <lordrosta@gmail.com>
2025-04-01 01:42:47 +0200

    Add login and signup pages with routing and css; include react-icons dependency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import React from 'react';
 import './App.css';
+import Login from "./pages/login"; 
+import Signup from "./pages/signup"; 
 import Intro from './pages/intro';
 
 function App() {
@@ -8,6 +10,9 @@ function App() {
         <Router>
         <Routes>
             <Route path="/" element={<Intro />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
         </Router>
     );
