578bcd57304662b24c5ff34ab99cba9750b11fcc
mukbeast78 <mazbersin@>
2025-04-01 16:21:13 +0200

    fix

---
+++ frontend/src/pages/intro/auth/signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from '../../components/signup/signup.jsx';
+
+const SignupPage = () => {
+    return (
+        <div>
+            <Login />
+        </div>
+    );
+};
+
+export default SignupPage;
