c141c56c319146e831885321a00375628c7f3a89
lordrosta-r <lordrosta@gmail.com>
2025-04-01 02:06:48 +0200

    Format code in App.jsx for improved readability

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import React from 'react';
 import './App.css';
-import Login from "./pages/login"; 
-import Signup from "./pages/signup"; 
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 import Intro from './pages/intro';
 
 function App() {
     return (
         <Router>
-        <Routes>
-            <Route path="/" element={<Intro />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
+            <Routes>
+                <Route path="/" element={<Intro />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
         </Router>
     );
 }
