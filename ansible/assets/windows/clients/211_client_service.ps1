94a103884a07e74e5ac2dbdacb5a086faa61b59c
[CorentinBonneau] <[corentin_bonneau@hotmail.fr]>
2025-06-04 11:56:14 +0200

    update client 211

--- ansible/assets/windows/clients/211_client_service.ps1
+++ ansible/assets/windows/clients/211_client_service.ps1
@@ -1,13 +1,15 @@
 # Définir le chemin du répertoire
 $servicePath = "C:\Users\svc-winrm\Desktop\service path"
 
+Copy-Item -Path "D:\info.exe" -Destination $servicePath -Force
+
 # Créer le répertoire si nécessaire
 if (!(Test-Path -Path $servicePath)) {
     New-Item -ItemType Directory -Path $servicePath
 }
 
-# Créer le contenu du fichier batch
-$batchContent = '@echo off' + [Environment]::NewLine + 'ver > "' + $servicePath + '\version.txt"'
+# Commande pour créer le service
+sc.exe create "info_win" binPath= "$servicePath\info.exe" DisplayName= "Infos Windows" start=auto
 
-# Créer le fichier batch
-Set-Content -Path "$servicePath\test.bat" -Value $batchContent
+# Démarrer le service
+Start-Service -Name "info_win"
