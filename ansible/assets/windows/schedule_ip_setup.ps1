229e65c3979962de93a0ac6a24e7014c636c0c67
Valentin <43043885+itsmrval@users.noreply.github.com>
2025-06-05 11:16:04 +0200

    fix(build) ip setup task

--- ansible/assets/windows/schedule_ip_setup.ps1
+++ ansible/assets/windows/schedule_ip_setup.ps1
@@ -1,11 +1,11 @@
-$scriptPath = "C:\script\setup_ip.ps1"
+$scriptPath = "C:\script\ip_setup.ps1"
 
 $destinationDir = "C:\script"
 if (!(Test-Path -Path $destinationDir)) {
     New-Item -Path $destinationDir -ItemType Directory -Force
 }
 
-Copy-Item -Path "D:\setup_ip.ps1" -Destination $scriptPath -Force
+Copy-Item -Path "D:\ip_setup.ps1" -Destination $scriptPath -Force
 
 $TaskName = "ExecuteSetupIPAtStartup"
 $TaskAction = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
@@ -17,4 +17,4 @@ $TaskSettings.AllowStartIfOnBatteries = $true
 $TaskSettings.DisallowStartIfOnBatteries = $false
 $TaskSettings.RunOnlyIfNetworkAvailable = $false
 
-Register-ScheduledTask -TaskName $TaskName -Action $TaskAction -Trigger $TaskTrigger -Settings $TaskSettings -RunLevel Highest -Force
+Register-ScheduledTask -TaskName $TaskName -Action $TaskAction -Trigger $TaskTrigger -Settings $TaskSettings -User "SYSTEM" -RunLevel Highest -Force
